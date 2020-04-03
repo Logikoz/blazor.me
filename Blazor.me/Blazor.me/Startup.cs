4624a64cce4c2f97795a7da526edadca04a5617f
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-03 08:26:59 -0500

    ✨ Corrigindo problemas nas versoes.

---
+++ Blazor.me/Blazor.me/Startup.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor.me
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IComponentsApplicationBuilder app) => app.AddComponent<App>("app");
+    }
+}
